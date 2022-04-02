0535cdfd548d35fca2323139afc3ad7fb97fe02d
sowmik-sec <48240954+sowmik-sec@users.noreply.github.com>
2022-04-03 00:56:18 +0600

    Matrix diagonal sum's solution added

---
+++ Array/Easy/1572. Matrix Diagonal Sum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int ans = 0;
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<n;j++) {
+                if(i==j) ans+=mat[i][j];
+                if((i+j)==n-1 and i!=j) ans+=mat[i][j];
+            }
+        }
+        return ans;
+    }
+};
