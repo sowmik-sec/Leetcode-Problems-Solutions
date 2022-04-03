3433ada31e80b357c4e0c092926d6ae1a4b606dc
sowmik-sec <48240954+sowmik-sec@users.noreply.github.com>
2022-04-03 15:47:05 +0600

    1217 solved using solution

---
+++ Array/Easy/1217. Minimum Cost to Move Chips to The Same Position.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minCostToMoveChips(vector<int>& position) {
+        int n = position.size();
+        int odd = 0, even = 0;
+        for(int i=0;i<n;i++) {
+            if(position[i]%2)++odd;
+            else even++;
+        }
+        return min(odd,even);
+    }
+};
