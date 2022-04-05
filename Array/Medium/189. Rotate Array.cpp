1fd19c3568cb75d9f4eb90c6e093e9172392db59
sowmik-sec <48240954+sowmik-sec@users.noreply.github.com>
2022-04-06 00:44:12 +0600

    Solved with O(1) space complexity

--- Array/Medium/189. Rotate Array.cpp
+++ Array/Medium/189. Rotate Array.cpp
@@ -3,9 +3,8 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
         k%=n;
-        vector<int> ans;
-        for(int i=n-k;i<n;i++) ans.push_back(nums[i]);
-        for(int i=0;i<n-k;i++) ans.push_back(nums[i]);
-        nums = ans;
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
     }
 };
