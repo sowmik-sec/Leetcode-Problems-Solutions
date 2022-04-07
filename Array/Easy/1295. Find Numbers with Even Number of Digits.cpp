f2bcc363cba5987b45f743a30f6e639826e70eca
sowmik-sec <48240954+sowmik-sec@users.noreply.github.com>
2022-04-07 14:42:03 +0600

    Better solution

--- Array/Easy/1295. Find Numbers with Even Number of Digits.cpp
+++ Array/Easy/1295. Find Numbers with Even Number of Digits.cpp
@@ -1,19 +1,11 @@
 class Solution {
 public:
-    int count_digit(int n)
-    {
-        int cnt = 0;
-        while(n) {
-            cnt++;
-            n/=10;
-        }
-        return cnt;
-    }
     int findNumbers(vector<int>& nums) {
         int n = nums.size();
         int ans = 0;
         for(int i=0;i<n;i++) {
-            if(count_digit(nums[i])%2==0)++ans;
+            int temp = log10(nums[i])+1;
+            if(temp%2==0)++ans;
         }
         return ans;
     }
