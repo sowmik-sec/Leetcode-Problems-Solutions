e6ab78e057845b0aa7a4387b4870d543ddae78a1
sowmik-sec <48240954+sowmik-sec@users.noreply.github.com>
2022-04-08 15:46:52 +0600

    Discussion

--- Search(linear & Binary)/Easy/1539. Kth Missing Positive Number.cpp
+++ Search(linear & Binary)/Easy/1539. Kth Missing Positive Number.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
-        int n = arr.size();
-        vector<int> v(1111),a;
-        
-        for(int i=0;i<n;i++) v[arr[i]]++;
-        for(int i = 1;i<arr[n-1]+1;i++)
-            if(!v[i])
-                a.push_back(i);
-        if(k>a.size()) return arr[n-1]+ (k-a.size());
-        return a[k-1];
+        int l = 0, r = arr.size()-1;
+        while(l<=r) {
+            int mid = l + (r-l)/2;
+            if(arr[mid]-1-mid<k) l = mid+1;
+            else r = mid-1;
+        }
+        return l + k;
     }
 };
