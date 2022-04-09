3cfa1331333285cf50f6c5ef039359b61db068dd
sowmik-sec <48240954+sowmik-sec@users.noreply.github.com>
2022-04-09 23:13:02 +0600

    Raw code

--- Search(linear & Binary)/Easy/1608. Special Array With X Elements Greater Than or Equal X.cpp
+++ Search(linear & Binary)/Easy/1608. Special Array With X Elements Greater Than or Equal X.cpp
@@ -6,8 +6,13 @@ public:
         for(int i=0;i<=mx;i++) {
             int l = 0, r = nums.size()-1;
             int n = r+1;
-            int low = lower_bound(nums.begin(),nums.end(),i)-nums.begin();
-            if(i==n-low) return i;
+            //int low = lower_bound(nums.begin(),nums.end(),i)-nums.begin();
+            while(l<=r) {
+                int mid = l + (r-l)/2;
+                if(i>nums[mid]) l = mid + 1;
+                else r = mid - 1;
+            }
+            if(i==n-l) return i;
         }
         return -1;
     }
