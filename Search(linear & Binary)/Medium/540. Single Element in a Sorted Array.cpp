fe043561d7c19fdb0b597223fa852ea89ab69d8c
sowmik-sec <48240954+sowmik-sec@users.noreply.github.com>
2022-04-10 23:42:16 +0600

    binary search solution

--- Search(linear & Binary)/Medium/540. Single Element in a Sorted Array.cpp
+++ Search(linear & Binary)/Medium/540. Single Element in a Sorted Array.cpp
@@ -1,8 +1,19 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int ans = 0, n = nums.size();
-        for(int i=0;i<n;i++) ans^=nums[i];
-        return ans;
+        int l = 0, r = nums.size()-1;
+        if(r==0) return nums[0];
+        while(l<r) {
+            int mid = l + (r-l)/2;
+            if(mid+1<nums.size() and mid-1>=0 and nums[mid]!=nums[mid-1] and nums[mid]!=nums[mid+1]) {
+                l = mid;
+                break;
+            }
+            if(mid%2==0 and mid+1<nums.size() and nums[mid]!=nums[mid+1]) r = mid - 1;
+            else if(mid%2 and mid>=0 and nums[mid]!=nums[mid-1])  r = mid - 1;
+            else l = mid + 1;
+        }
+        //cout<<l<<" "<<r<<endl;
+        return nums[l];
     }
 };
