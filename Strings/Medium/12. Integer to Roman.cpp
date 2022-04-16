a7cad7c004652347874033b3eb1061c4b1b3426e
sowmik-sec <48240954+sowmik-sec@users.noreply.github.com>
2022-04-16 15:25:52 +0600

    Simple code

--- Strings/Medium/12. Integer to Roman.cpp
+++ Strings/Medium/12. Integer to Roman.cpp
@@ -1,62 +1,11 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        map<int,char> m;
-        string ans = "";
-        while(num>0) {
-            if(num>=1000) {
-                ans += "M";
-                num-=1000;
-            }
-            else if(num>=900) {
-                ans += "CM";
-                num -= 900;
-            }
-            else if(num>=500) {
-                ans += "D";
-                num -= 500;
-            }
-            else if(num>=400) {
-                ans += "CD";
-                num -= 400;
-            }
-            else if(num>=100) {
-                ans += "C";
-                num -= 100;
-            }
-            else if(num>=90) {
-                ans += "XC";
-                num -= 90;
-            }
-            else if(num>=50) {
-                ans += "L";
-                num -= 50;
-            }
-            else if(num>=40) {
-                ans += "XL";
-                num -= 40;
-            }
-            else if(num>=10) {
-                ans += "X";
-                num -= 10;
-            }
-            else if(num>=9) {
-                ans += "IX";
-                num -= 9;
-            }
-            else if(num>=5) {
-                ans += "V";
-                num -= 5;
-            }
-            else if(num>=4) {
-                ans += "IV";
-                num -=4;
-            }
-            else {
-                ans += "I";
-                num--;
-            }
-        }
+        vector<string> I = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+        vector<string> X = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+        vector<string> C = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+        vector<string> M = {"","M","MM","MMM"};
+        string ans = M[num/1000] + C[(num%1000)/100] + X[(num%100)/10] + I[num%10];
         return ans;
     }
 };
