c22e38128b32a3c31e08c18aefcbd3cbf38de26f
sowmik-sec <48240954+sowmik-sec@users.noreply.github.com>
2022-04-18 15:13:24 +0600

    Using XOR. Solved Alhamdulillah.

--- Sorting/Easy/268. Missing Number.cpp
+++ Sorting/Easy/268. Missing Number.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int missingNumber(vector<int>& a) {
-        unordered_map<int,int> m;
-        int n = a.size();
-        for(int i=0;i<n;i++) m[a[i]]++;
-        for(int i=0;i<=n+1;i++) {
-            if(!m[i]) return i;
+        int res = a.size();
+        for(int i=0;i<a.size();i++) {
+            res^=i;
+            res^=a[i];
         }
-        return 0;
+        return res;
     }
 };
