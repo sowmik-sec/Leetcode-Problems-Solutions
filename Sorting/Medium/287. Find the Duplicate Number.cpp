936c7cf2fae32e43ee1b09bfe96c19dcabbb053a
sowmik-sec <48240954+sowmik-sec@users.noreply.github.com>
2022-04-26 13:12:54 +0600

    Clean code with cycle sort. Solved Alhamdulillah

--- Sorting/Medium/287. Find the Duplicate Number.cpp
+++ Sorting/Medium/287. Find the Duplicate Number.cpp
@@ -3,13 +3,13 @@ public:
     int findDuplicate(vector<int>& nums) {
         int i = 0;
         while(i<nums.size()) {
-            if(nums[i]<nums.size() and nums[i]!=nums[nums[i]-1]) swap(nums[i], nums[nums[i]-1]);
+            if(nums[i]!=i+1) {
+                if(nums[i]!=nums[nums[i]-1]) swap(nums[i],nums[nums[i]-1]);
+                else return nums[i];
+            }
             else i++;
         }
-        for(int j=0;j<nums.size();j++) {
-            if(j+1!=nums[j]) return nums[j];
-        }
         
-        return nums.size();
+        return -1;
     }
 };
