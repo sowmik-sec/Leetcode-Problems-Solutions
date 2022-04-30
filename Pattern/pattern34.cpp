265e26aca32a2293aa23195c1c0c158093cf79e1
sowmik-sec <48240954+sowmik-sec@users.noreply.github.com>
2022-05-01 00:14:16 +0600

    Solved Alhamdulillah

---
+++ Pattern/pattern34.cpp
@@ -0,0 +1,56 @@
+/******* BISMILLAHIR RAHMANIR RAHIM *******/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+#define ll long long int
+#define u unsigned
+#define vii vector<ll>
+#define pb push_back
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+#define sz size()
+#define precision cout<<setprecision(10)<<fixed
+#define tt int t;cin>>t;while(t--)
+#define loop(n) for(int i=0;i<n;i++)
+#define lop(a,n) for(int i=a;i<=n;i++)
+#define rloop(n) for(int i=n-1;i>=0;i--)
+#define read freopen("input.txt","r",stdin)
+#define write freopen("output.txt","w",stdout)
+#define ioboost ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+bool gre(ll a,ll b){return a>b;}
+
+/**
+
+34.    E D C B A
+       D C B A
+       C B A
+       B A
+       A
+
+*/
+
+void pattern(int n)
+{
+    int cnt = 1;
+    char c = 'A';
+    for(int row=1;row<=n;row++) {
+        int nofcol, noofspace, inspace;
+        nofcol = row<=n? row: 2*n-row;
+        noofspace = row<=n? (n-row)*2 : (row-n)*2;
+        //inspace = row<=n? row*2-3 : (n-(row-n))*2-3;
+        for(int col=1;col<=n-row+1;col++) cout<<char(c+(n-col-row+1))<<" ";
+        cout<<endl;
+    }
+}
+
+int32_t main()
+{
+    ioboost;
+    int n;cin>>n;
+    pattern(n);
+    return 0;
+}
+
+/*****************  ALHAMDULILLAH  *****************/
