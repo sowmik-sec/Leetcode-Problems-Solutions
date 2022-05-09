9d9f4944c81dd2d5b77826cec7c37732ca4eb225
sowmik-sec <48240954+sowmik-sec@users.noreply.github.com>
2022-05-09 11:27:45 +0600

    Using merge sort raw code solved Alhamdulillah

--- Sorting/Medium/215. Kth Largest Element in an Array.cpp
+++ Sorting/Medium/215. Kth Largest Element in an Array.cpp
@@ -1,7 +1,25 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        sort(nums.begin(),nums.end());
-        return nums[nums.size()-k];
+        merge_sort(nums,0,nums.size());
+        return nums[k-1];
+    }
+    void merge(vector<int> &a, int l, int m, int h) {
+        vector<int> temp(h-l);
+        int i = l, j = m, k = 0;
+        while(i<m and j<h) {
+            if(a[i]>a[j]) temp[k++] = a[i++];
+            else temp[k++] = a[j++];
+        }
+        while(i<m) temp[k++] = a[i++];
+        while(j<h) temp[k++] = a[j++];
+        for(int it=0;it<temp.size();it++) a[l+it] = temp[it];
+    }
+    void merge_sort(vector<int> &a, int l, int h) {
+        if(h-l==1) return;
+        int m = l + (h-l)/2;
+        merge_sort(a,l,m);
+        merge_sort(a,m,h);
+        merge(a,l,m,h);
     }
 };
