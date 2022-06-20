ee258a93a8b35cce290fede47332a4e502af5879
sowmik-sec <48240954+sowmik-sec@users.noreply.github.com>
2022-06-21 01:48:59 +0600

    This is a currect solution Alhamdulillah

--- Recursion/Medium/Dice.java
+++ Recursion/Medium/Dice.java
@@ -1,3 +1,5 @@
+package kunal.recursion.backtracking;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -5,30 +7,36 @@ import java.util.Scanner;
 public class Dice {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        List<ArrayList<Integer>> ans = dice(new ArrayList<>(),n,n);
+        int n = 4;
+        int k = 6;
+        //List<String> ans = dice("",n);
         //System.out.println(ans);
+        List<String> df = diceFace("",n,k);
     }
 
-    private static List<ArrayList<Integer>> dice(ArrayList<Integer> p, int up,int target) {
-        List<ArrayList<Integer>> list = new ArrayList<>();
-        int sum = 0;
-        for (int i = 0; i < p.size(); i++) {
-            sum += p.get(i);
-        }
-        if(sum==target) {
-            list.add(p);
-            //System.out.println(list + " in sum");
-            return list;
-        }
+    private static List<String> dice(String p, int up) {
+        List<String> list = new ArrayList<>();
         if(up==0) {
-            //System.out.println(list + " in up");
+            list.add(p);
             return list;
         }
-        for (int i = 1; i <= up; i++) {
-            p.add(i);
-            System.out.println(p);
-            list.addAll(dice(p,up-i, target));
+
+        for (int i = 1; i <= up && i<=6; i++) {
+            list.addAll (dice(p+i,up-i));
+            //System.out.println(list);
+        }
+        //System.out.println(list);
+        return list;
+    }
+    private static List<String> diceFace(String p, int up, int face) {
+        List<String> list = new ArrayList<>();
+        if(up==0) {
+            list.add(p);
+            return list;
+        }
+
+        for (int i = 1; i <= up && i<=face; i++) {
+            list.addAll (diceFace(p+i,up-i,face));
             //System.out.println(list);
         }
         //System.out.println(list);
